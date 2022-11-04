be1680390650d153a478b2bfbb39edc84ae2ad6f
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2022-11-04 10:35:03 +0300

    Update catch_ball_game_5.py

--- first_lab/catch_ball_game_5.py
+++ first_lab/catch_ball_game_5.py
@@ -26,20 +26,26 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 COLORS_FILLED = [BLACK]*6
 
 
-def new_ball(colors):
-    '''рисует новый шарик '''
+def new_ball():
+    '''рисует новый шарик 
+    circle_x, circle_y, circle_r, color - параметры шарика
+    v_circle_x, v_circle_y - параметры вектора скорости шарика
+    Возвращает все параметры для одного шара'''
     circle_x = randint(100, 900)
     circle_y = randint(100, 900)
     circle_r = randint(10, 100)
     v_circle_x = randint(-10, 10)
     v_circle_y = randint(-10, 10)
-    color = colors[randint(0, 5)]
+    color = COLORS[randint(0, 5)]
     pygame.draw.circle(screen, color, (circle_x, circle_y), circle_r)
     return circle_x, circle_y, circle_r, v_circle_x, v_circle_y, color
 
 
 def scores(circle_cords, mouse_x, mouse_y):
-    '''Эта функция анализирует, попал ли игрок по шарику'''
+    '''Эта функция анализирует, попал ли игрок по шарику
+    Получает на вход:
+    circle_cords - координаты всех шаров
+    mouse_x, mouse_y - координаты мыши'''
     for circle_x, circle_y, circle_r, v_circle_x, v_circle_y, color in circle_cords:
         if (mouse_x - circle_x)**2 + (mouse_y - circle_y)**2 <= circle_r**2:
             return True
@@ -47,15 +53,17 @@ def scores(circle_cords, mouse_x, mouse_y):
 
 
 def create_balls(n):
-    '''Эта функция создает n шаров'''
+    '''Эта функция создает n шаров и возвращает их координаты'''
     circle_cords = []
     for _ in range(n):
-        circle_cords.append((new_ball(COLORS)))
+        circle_cords.append((new_ball()))
     return circle_cords
 
 
 def create_balls_with_moving(circle_cords):
-    '''Эта фунция добавляет шарам движение'''
+    '''Эта фунция добавляет шарам движение
+    circle_cords - координаты всех шаров
+    Возвращает новые координаты шаров с учетом движения'''
     new_circle_cords = []
     for circle_x, circle_y, circle_r, v_circle_x, v_circle_y, color in circle_cords:
         pygame.draw.circle(screen, BLACK, (circle_x, circle_y), circle_r)
@@ -70,6 +78,10 @@ def create_balls_with_moving(circle_cords):
 
 
 def write_score_to_csv_file(USER_NAME, score):
+    ''' Эта функция обновляет очки для отдельных игроков после каждой игры
+    USER_NAME - никнейм игрока,
+    score - количество очков, которое он получил
+    '''
     df = pd.read_csv('scores.csv')
     print(list(df))
     if USER_NAME not in list(df.NAME):
